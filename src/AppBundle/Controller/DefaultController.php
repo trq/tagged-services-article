1439d4b1315c954233335474a616b25307cb955e
Tony R Quilkey <trq@thorpesystems.com>
2015-07-23 22:00:49 +1000

    Add RuleManager service

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\RuleManager\IsNumericRule;
+use AppBundle\RuleManager\LessThanRule;
+use AppBundle\RuleManager\GreaterThanRule;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -13,6 +16,13 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $data = [2000,4000,8000,10000, 'abc', 20000, 40000, 'xyz'];
+
+        $ruleManager = $this->get('app.rule_manager');
+        $ruleManager->addRule(new IsNumericRule());
+        $ruleManager->addRule(new GreaterThanRule());
+        $ruleManager->addRule(new LessThanRule());
+
+        $data = $ruleManager->applyRules($data);
 
         return $this->render('default/index.html.twig', ['data' => $data]);
     }
