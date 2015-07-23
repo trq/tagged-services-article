2edcf179d60860c85320b795349d5aca07e13874
Tony R Quilkey <trq@thorpesystems.com>
2015-07-23 23:11:18 +1000

    Add the bulk of the service config.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\RuleManager\IsNumericRule;
-use AppBundle\RuleManager\LessThanRule;
-use AppBundle\RuleManager\GreaterThanRule;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -17,12 +14,7 @@ class DefaultController extends Controller
     {
         $data = [2000,4000,8000,10000, 'abc', 20000, 40000, 'xyz'];
 
-        $ruleManager = $this->get('app.rule_manager');
-        $ruleManager->addRule(new IsNumericRule());
-        $ruleManager->addRule(new GreaterThanRule());
-        $ruleManager->addRule(new LessThanRule());
-
-        $data = $ruleManager->applyRules($data);
+        $data = $this->get('app.rule_manager')->applyRules($data);
 
         return $this->render('default/index.html.twig', ['data' => $data]);
     }
